08d5f873ae9c4f0139622883777690e2e2d8701a
Baoro <mam.baoro@outlook.fr>
2019-07-03 21:47:06 +0200

    ready for heroku

--- app/components/Documentation/index.js
+++ app/components/Documentation/index.js
@@ -9,7 +9,6 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import { Router } from '@reach/router';
 import { createBrowserHistory } from 'history';
-import posed, { PoseGroup } from 'react-pose';
 import Introduction from '../Introduction/Loadable';
 import WhatYou from '../WhatYou/Loadable';
 import JavaScriptJava from '../JavaScriptJava/Loadable';
@@ -27,16 +26,6 @@ const css1Bp600 = css`
   width: 95%;
 `;
 
-const RoutesContainer = posed.div({
-  enter: {
-    opacity: 1,
-    delay: 100,
-    beforeChildren: true,
-    transition: { duration: 100 },
-  },
-  exit: { opacity: 0, transition: { duration: 50 } },
-});
-
 const DivDoc = styled.div`
   width: 100%;
   margin: 3rem;
@@ -48,22 +37,18 @@ function Documentation() {
   const { location } = history;
   return (
     <DivDoc>
-      <PoseGroup>
-        <RoutesContainer key={location}>
-          <Router location={location}>
-            <Introduction path="introduction" default />
-            <WhatYou path="whatyou" />
-            <JavaScriptJava path="javascript-java" />
-            <HelloWorld path="hello-world" />
-            <Variables path="variables" />
-            <DeclaringVar path="declaring-var" />
-            <VariableScope path="variable-scope" />
-            <GlobalVar path="global-var" />
-            <Constants path="constants" />
-            <DataTypes path="datatypes" />
-          </Router>
-        </RoutesContainer>
-      </PoseGroup>
+      <Router location={location}>
+        <Introduction path="introduction" default />
+        <WhatYou path="whatyou" />
+        <JavaScriptJava path="javascript-java" />
+        <HelloWorld path="hello-world" />
+        <Variables path="variables" />
+        <DeclaringVar path="declaring-var" />
+        <VariableScope path="variable-scope" />
+        <GlobalVar path="global-var" />
+        <Constants path="constants" />
+        <DataTypes path="datatypes" />
+      </Router>
     </DivDoc>
   );
 }
